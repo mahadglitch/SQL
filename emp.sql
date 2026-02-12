34549c104a78bc3b28f903c50c8b30053a760ace
mahadglitch <mahadbinkyser@gmail.com>
2026-02-12 23:00:40 +0600

    hi

--- emp.sql
+++ emp.sql
@@ -11,10 +11,15 @@ dno char(3)
 
 insert into empl VALUES
 ('E01','Mahad',15, 'M', 50000, 2, 'Dhaka', 'D05'),
-('E02','Tasnim',13, 'F', 40000, 1, 'Null', 'D02'),
+('E02','Tasnim',13, 'F', 40000, 1, NULL, 'D02'),
 ('E03','Rahim',35, 'M', 20000, 2, 'Dhaka', 'D07'),
 ('E04','Zarin',13, 'F', 60000, 5, 'Cumilla', 'D04'),
 ('E05','Araf',15, 'M', 10000, 3, 'Rajshahi', 'D06'),
 ('E06','Ayman',16, 'M', 800000, 2, 'Cumilla', 'D01'),
 ('E07','Farhan',23, 'M', 50000, 4, 'Rajshahi', 'D03')
 ;
+
+select * from empl;
+
+# Display the names of employees
+select ename from empl;
---
+++ student.sql
@@ -0,0 +1,14 @@
+create table student
+(
+rno tinyint(2),
+name varchar(15),
+age tinyint(2),
+gen char(1),
+grade tinyint(2),
+percent decimal(5,2)
+);
+
+
+select * from student;
+
+
